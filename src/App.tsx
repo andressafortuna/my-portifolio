790e4b3097a3f811872ac6ada1f92eb899482a7d
andressafortuna <andressafortuna1@gmail.com>
2025-05-17 23:25:34 -0300

    creating screen skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,76 @@
-import './App.css'
+import { useState, useRef } from "react";
+import { MdOutlineLanguage } from "react-icons/md";
+import Content from "../src/components/Content";
 
-function App() {
-  // const [count, setCount] = useState(0)
+export default function App() {
+  const [language, setLanguage] = useState<"pt" | "en">("pt");
+  
 
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
+  const toggleLanguage = () => {
+    setLanguage((prev) => (prev === "pt" ? "en" : "pt"));
+  };
+
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const workRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = (ref: React.RefObject<HTMLDivElement | null>) => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const texts = {
+    pt: {
+      about: "Sobre",
+      experience: "Experiência",
+      contact: "Entre em contato"
+    },
+    en: {
+      about: "About me",
+      experience: "Work",
+      contact: "Get in touch"
+    }
+  };
+
+  const t = texts[language];
+
   return (
-    <main className="min-h-screen bg-gray-100 p-8">
-      <section className="max-w-3xl mx-auto bg-white rounded-xl shadow-md p-6">
-        <h1 className="text-3xl font-bold text-blue-600">Olá, eu sou a Andressa!</h1>
-        <p className="mt-4 text-gray-700">
-          Desenvolvedora Frontend com experiência em Angular + .NET e estudando React + Tailwind.
-        </p>
-      </section>
-    </main>
-  );
-}
+    <div className="flex h-screen bg-white transition-colors">
 
-export default App
+      <div className="flex flex-col flex-1">
+
+        <nav className="flex items-center justify-between p-4 bg-white shadow">
+          <h2 className="text-4xl font-bold">Andressa</h2>
+
+          <div className="flex items-center gap-4">
+            <button onClick={toggleLanguage} className="text-[#2e2e2e] hover:text-[#ff9c00]">
+              <MdOutlineLanguage className="w-6 h-6" />
+            </button>
+            <button onClick={() => scrollToSection(aboutRef)} className="font-bold hover:underline">
+              {t.about}
+            </button>
+            <button onClick={() => scrollToSection(workRef)} className="font-bold hover:underline">
+              {t.experience}
+            </button>
+            <button
+              onClick={() => scrollToSection(contactRef)}
+              className="text-white font-bold bg-[#ff9c00] px-4 py-2 rounded-full"
+            >
+              {t.contact}
+            </button>
+          </div>
+        </nav >
+
+        <div className="bg-gray-100 overflow-y-auto flex-1">
+          <Content
+            language={language}
+            aboutRef={aboutRef}
+            workRef={workRef}
+            contactRef={contactRef}
+          />
+        </div>
+
+      </div>
+    </div>
+  );
+
+}
